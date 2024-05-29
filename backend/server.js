b90f5e919e7b9be7fd637efbf3d2fdc2b969a664
ImMateus <mateuslealwork@gmail.com>
2024-05-29 11:46:39 -0300

    Signup Endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,12 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
 
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Hello");
-})
+
+app.use(express.json()); 
 
 app.use("/api/auth", authRoutes);
 
